a5c7a44fd0b2dc7f3bb9a2897d3757e9dd1bc764
kovidomi <kovidomi01@citromail.hu>
2018-11-01 16:03:42 +0100

    Updated CharInfo

--- src/Game/CharInfo.h
+++ src/Game/CharInfo.h
@@ -6,9 +6,19 @@ class CharInfo
 public:
 	char pad_0000[52]; //0x0000
 	uint32_t char_index; //0x0034
-	char pad_0038[368]; //0x0038
+	char pad_0038[92]; //0x0038
+	uint32_t total_hitboxes; //0x0094
+	uint32_t total_hurtboxes; //0x0098
+	char pad_009C[60]; //0x009C
+	char* current_sprite; //0x00D8
+	char pad_00DC[204]; //0x00DC
 	uint32_t chie_charge_lvl; //0x01A8
-	char pad_01AC[2296]; //0x01AC
+	char pad_01AC[204]; //0x01AC
+	int32_t position_x; //0x0278
+	int32_t position_y; //0x027C
+	char pad_0280[1108]; //0x0280
+	uint32_t deny_opponent_burst; //0x06D4
+	char pad_06D8[972]; //0x06D8
 	uint32_t cur_hp; //0x0AA4
 	uint32_t max_hp; //0x0AA8
 	char pad_0AAC[7504]; //0x0AAC
