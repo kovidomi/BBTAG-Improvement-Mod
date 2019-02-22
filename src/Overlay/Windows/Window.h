d7f1396d165367780fcbef58a8e750999267056a
kovidomi <kovidomi01@citromail.hu>
2019-02-22 12:17:20 +0100

    Add potentially useful methods

--- src/Overlay/Windows/Window.h
+++ src/Overlay/Windows/Window.h
@@ -2,6 +2,12 @@
 
 class Window
 {
+	// bool m_windowOpen;
+
+	// virtual void PushStyles() = 0;
+	// virtual void PopStyles() = 0;
+	// void SavePreviousGlobalStyles();
+	// void RestorePreviousGlobalStyles();
 public:
 	virtual void Show() = 0;
 	virtual ~Window() {};
