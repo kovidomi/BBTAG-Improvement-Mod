4d0fa94735b8ff56aa9d5565bfd81871f05e9316
kovidomi <kovidomi01@citromail.hu>
2019-02-25 19:09:51 +0100

    Add class methods and members

--- src/Overlay/Windows/Window.h
+++ src/Overlay/Windows/Window.h
@@ -1,14 +1,26 @@
 #pragma once
+#include <imgui.h>
+#include <string>
 
 class Window
 {
-	// bool m_windowOpen;
-
 	// virtual void PushStyles() = 0;
 	// virtual void PopStyles() = 0;
 	// void SavePreviousGlobalStyles();
 	// void RestorePreviousGlobalStyles();
 public:
-	virtual void Show() = 0;
+	virtual void BeforeDraw() = 0;
+	virtual void Draw() = 0;
+	virtual void AfterDraw() = 0;
 	virtual ~Window() = default;
+	Window(std::string title, bool closable, ImGuiWindowFlags windowFlags);
+	void Update();
+	void Open();
+	void Close();
+
+protected:
+	ImGuiWindowFlags m_windowFlags;
+	bool m_windowOpen = false;
+	bool m_windowClosable;
+	std::string m_windowTitle;
 };
