9f1354c0a05df762b298a968fca7c6899b7e7f9d
kovidomi <kovidomi01@citromail.hu>
2019-03-01 01:41:01 +0100

    Add test drawing functions

--- src/WindowManager/HitboxOverlay.cpp
+++ src/WindowManager/HitboxOverlay.cpp
@@ -1,17 +1,16 @@
 #include "HitboxOverlay.h"
-#include "../interfaces.h"
-#include "../Settings.h"
+
+#include "imgui_internal.h"
 
 HitboxOverlay::HitboxOverlay()
 {
-	m_resolution = ImVec2(Settings::settingsIni.renderwidth, Settings::settingsIni.renderheight);
 }
 
 void HitboxOverlay::Update()
 {
 	BeforeDraw();
 
-	ImGui::Begin("Hitbox Overlay", NULL, m_windowFlags);
+	ImGui::Begin("##HitboxOverlay", nullptr, m_windowFlags);
 
 	Draw();
 
@@ -22,21 +21,93 @@ void HitboxOverlay::Update()
 
 void HitboxOverlay::BeforeDraw()
 {
-	ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
-	ImGui::SetNextWindowSize(m_resolution);
-
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0.0f, 0.0f });
 	ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.0f, 0.0f, 0.0f, 0.0f));
+
+	ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
+
+	ImGuiIO& io = ImGui::GetIO();
+	ImGui::SetNextWindowSize(ImVec2(io.DisplaySize.x, io.DisplaySize.y));
 }
 
 void HitboxOverlay::Draw()
 {
-	ImGui::Text("TEST TEST TEST TEST TEST ");
-	ImGui::Text("TEST TEST TEST TEST TEST ");
-	ImGui::Text("TEST TEST TEST TEST TEST ");
-	ImGui::Text("TEST TEST TEST TEST TEST ");
+	ImGui::SliderFloat4("Box", &m_rectAx, 0.0f, 1600.0f, "%.0f");
+
+	RenderLine(m_lineFrom, m_lineTo, 0xFFFF9900, 2);
+	RenderRect(ImVec2(m_rectAx, m_rectAy), ImVec2(m_rectBx, m_rectBy), 0xFF0033CC, 0, ImDrawCornerFlags_All, 2);
 }
 
 void HitboxOverlay::AfterDraw()
 {
 	ImGui::PopStyleColor();
+	ImGui::PopStyleVar(2);
+}
+
+void HitboxOverlay::SetChar1ScreenPos(float x, float y)
+{
+	m_lineFrom = ImVec2(x, y);
+	m_lineTo = ImVec2(x, y - 500);
+}
+
+
+void HitboxOverlay::RenderLine(const ImVec2& from, const ImVec2& to, uint32_t color, float thickness)
+{
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+
+	float a = (color >> 24) & 0xff;
+	float r = (color >> 16) & 0xff;
+	float g = (color >> 8) & 0xff;
+	float b = (color) & 0xff;
+
+	window->DrawList->AddLine(from, to, ImGui::GetColorU32({ r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f }), thickness);
+}
+
+void HitboxOverlay::RenderCircle(const ImVec2& position, float radius, uint32_t color, float thickness, uint32_t segments)
+{
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+
+	float a = (color >> 24) & 0xff;
+	float r = (color >> 16) & 0xff;
+	float g = (color >> 8) & 0xff;
+	float b = (color) & 0xff;
+
+	window->DrawList->AddCircle(position, radius, ImGui::GetColorU32({ r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f }), segments, thickness);
+}
+
+void HitboxOverlay::RenderCircleFilled(const ImVec2& position, float radius, uint32_t color, uint32_t segments)
+{
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+
+	float a = (color >> 24) & 0xff;
+	float r = (color >> 16) & 0xff;
+	float g = (color >> 8) & 0xff;
+	float b = (color) & 0xff;
+
+	window->DrawList->AddCircleFilled(position, radius, ImGui::GetColorU32({ r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f }), segments);
+}
+
+void HitboxOverlay::RenderRect(const ImVec2& from, const ImVec2& to, uint32_t color, float rounding, uint32_t roundingCornersFlags, float thickness)
+{
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+
+	float a = (color >> 24) & 0xFF;
+	float r = (color >> 16) & 0xFF;
+	float g = (color >> 8) & 0xFF;
+	float b = (color) & 0xFF;
+
+	window->DrawList->AddRect(from, to, ImGui::GetColorU32({ r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f }), rounding, roundingCornersFlags, thickness);
+}
+
+void HitboxOverlay::RenderRectFilled(const ImVec2& from, const ImVec2& to, uint32_t color, float rounding, uint32_t roundingCornersFlags)
+{
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+
+	float a = (color >> 24) & 0xFF;
+	float r = (color >> 16) & 0xFF;
+	float g = (color >> 8) & 0xFF;
+	float b = (color) & 0xFF;
+
+	window->DrawList->AddRectFilled(from, to, ImGui::GetColorU32({ r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f }), rounding, roundingCornersFlags);
 }
--- src/WindowManager/HitboxOverlay.h
+++ src/WindowManager/HitboxOverlay.h
@@ -1,22 +1,41 @@
 #pragma once
 #include <imgui.h>
 
+typedef unsigned int uint32_t;
+
 class HitboxOverlay
 {
 public:
 	HitboxOverlay();
 	void Update();
+	void SetChar1ScreenPos(float x, float y);
+
 private:
 	void BeforeDraw();
 	void Draw();
 	void AfterDraw();
 
+	void RenderLine(const ImVec2& from, const ImVec2& to, uint32_t color, float thickness = 1.0f);
+	void RenderCircle(const ImVec2& position, float radius, uint32_t color, float thickness = 1.0f, uint32_t segments = 16);
+	void RenderCircleFilled(const ImVec2& position, float radius, uint32_t color, uint32_t segments = 16);
+	void RenderRect(const ImVec2& from, const ImVec2& to, uint32_t color, float rounding = 0.0f, uint32_t roundingCornersFlags = ImDrawCornerFlags_All, float thickness = 1.0f);
+	void RenderRectFilled(const ImVec2& from, const ImVec2& to, uint32_t color, float rounding = 0.0f, uint32_t roundingCornersFlags = ImDrawCornerFlags_All);
+
+
 	ImGuiWindowFlags m_windowFlags =
 		ImGuiWindowFlags_AlwaysAutoResize
-			| ImGuiWindowFlags_NoTitleBar
-			| ImGuiWindowFlags_NoMove
-			| ImGuiWindowFlags_NoBringToFrontOnFocus
-			| ImGuiWindowFlags_NoFocusOnAppearing
-			| ImGuiWindowFlags_NoInputs;
-	ImVec2 m_resolution;
+		| ImGuiWindowFlags_NoTitleBar
+		| ImGuiWindowFlags_NoMove;
+			//| ImGuiWindowFlags_NoBringToFrontOnFocus
+			//| ImGuiWindowFlags_NoFocusOnAppearing
+			//| ImGuiWindowFlags_NoInputs;
+	// ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoInputs
+
+	float m_rectAx = 100.0f;
+	float m_rectAy = 100.0f;
+	float m_rectBx = 200.0f;
+	float m_rectBy = 200.0f;
+
+	ImVec2 m_lineFrom;
+	ImVec2 m_lineTo;
 };
