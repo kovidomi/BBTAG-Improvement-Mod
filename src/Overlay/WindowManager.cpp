527d11796a23608b87c2e33f2975e59bd7fc1d3b
kovidomi <kovidomi01@citromail.hu>
2019-03-10 22:08:18 +0100

    Move constructor to cpp, place dynamically allocated instances of windows in container

--- src/Overlay/WindowManager.cpp
+++ src/Overlay/WindowManager.cpp
@@ -1,4 +1,37 @@
 #include "WindowManager.h"
+#include "Core/logger.h"
+#include "Core/info.h"
+#include "Core/Settings.h"
+#include "Window/DebugWindow.h"
+#include "Window/DonatorsWindow.h"
+#include "Window/LogWindow.h"
+#include "Window/MainWindow.h"
+#include "Window/PaletteEditorWindow.h"
+#include "Window/UpdateNotifierWindow.h"
+
+WindowManager::WindowManager()
+{
+	InitWindows();
+	//m_middleScreen = ImVec2((float)Settings::settingsIni.renderwidth / 2, (float)Settings::settingsIni.renderheight / 2);
+
+	//dividing by 1904x1042 because the custom HUD was designed on that resolution
+	float hudScaleX = ((float)Settings::settingsIni.renderwidth * Settings::settingsIni.customhudscale) / 1904.0f;
+	float hudScaleY = ((float)Settings::settingsIni.renderheight * Settings::settingsIni.customhudscale) / 1042.0f;
+	LOG(2, "hud_scale_x: %f\n", hudScaleX);
+	LOG(2, "hud_scale_y: %f\n", hudScaleY);
+	m_customHud.SetScale(hudScaleX, hudScaleY);
+	m_showCustomHud = Settings::settingsIni.forcecustomhud;
+
+	GetWindow<MainWindow>(WindowType_Main)->SetWindowHandler(*this);
+}
+
+WindowManager::~WindowManager()
+{
+	for (auto window : m_windows)
+	{
+		delete window.second;
+	}
+}
 
 void WindowManager::DrawAllWindows()
 {
@@ -9,17 +42,33 @@ void WindowManager::DrawAllWindows()
 
 	if (m_showCustomHud)
 	{
-		m_customHud.OnUpdate(m_showCustomHud, m_mainWindow.IsOpen());
+		m_customHud.OnUpdate(m_showCustomHud, GetWindow(WindowType_Main)->IsOpen());
 	}
 }
 
-void WindowManager::InitWindowMap()
+void WindowManager::InitWindows()
 {
-	AddWindow(WindowType_Main, &m_mainWindow);
-	AddWindow(WindowType_Debug, &m_debugWindow);
-	AddWindow(WindowType_Log, &m_logWindow);
-	AddWindow(WindowType_Donators, &m_donatorsWindow);
-	AddWindow(WindowType_UpdateNotifier, &m_updateNotifierWindow);
-	AddWindow(WindowType_PaletteEditor, &m_paletteEditorWindow);
+	AddWindow(WindowType_Main,
+		new MainWindow(MOD_WINDOW_TITLE, false,
+			ImGuiWindowFlags_AlwaysAutoResize));
+
+	AddWindow(WindowType_Debug,
+		new DebugWindow("DEBUG", true));
+
+	AddWindow(WindowType_Log,
+		new LogWindow("Log", true,
+			ImGuiWindowFlags_NoCollapse));
+
+	AddWindow(WindowType_Donators,
+		new DonatorsWindow("##DONATORS", false,
+			ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse));
+
+	AddWindow(WindowType_UpdateNotifier,
+		new UpdateNotifierWindow("Update available", true,
+			ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse));
+
+	AddWindow(WindowType_PaletteEditor,
+		new PaletteEditorWindow("Palette Editor", true));
+
 	//m_windows[WindowType_CustomHud] = &m_customHud;
 }
--- src/Overlay/WindowManager.h
+++ src/Overlay/WindowManager.h
@@ -2,51 +2,15 @@
 #include "CustomHud.h"
 #include "WindowContainer.h"
 
-#include "Window/PaletteEditorWindow.h"
-#include "Window/LogWindow.h"
-#include "Window/UpdateNotifierWindow.h"
-#include "Window/DonatorsWindow.h"
-#include "Window/DebugWindow.h"
-#include "Window/MainWindow.h"
-
-#include "Core/info.h"
-#include "Core/logger.h"
-#include "Core/Settings.h"
-#include "imgui.h"
-
 class WindowManager : public WindowContainer
 {
 public:
-	WindowManager()
-	{
-		//m_middleScreen = ImVec2((float)Settings::settingsIni.renderwidth / 2, (float)Settings::settingsIni.renderheight / 2);
-
-		//dividing by 1904x1042 because the custom HUD was designed on that resolution
-		float hudScaleX = ((float)Settings::settingsIni.renderwidth * Settings::settingsIni.customhudscale) / 1904.0f;
-		float hudScaleY = ((float)Settings::settingsIni.renderheight * Settings::settingsIni.customhudscale) / 1042.0f;
-		LOG(2, "hud_scale_x: %f\n", hudScaleX);
-		LOG(2, "hud_scale_y: %f\n", hudScaleY);
-		m_customHud.SetScale(hudScaleX, hudScaleY);
-		m_showCustomHud = Settings::settingsIni.forcecustomhud;
-
-		m_mainWindow.SetWindowHandler(*this);
-		InitWindowMap();
-	}
-
-	~WindowManager() override = default;
+	WindowManager();
+	~WindowManager() override;
 	void DrawAllWindows();
 private:
-	void InitWindowMap();
-
-	MainWindow           m_mainWindow           = MainWindow(MOD_WINDOW_TITLE, false, ImGuiWindowFlags_AlwaysAutoResize);
-	DebugWindow          m_debugWindow          = DebugWindow("DEBUG", true);
-	DonatorsWindow       m_donatorsWindow       = DonatorsWindow("##DONATORS", false,
-		ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse);
-	LogWindow            m_logWindow            = LogWindow("Log", true, ImGuiWindowFlags_NoCollapse);
-	UpdateNotifierWindow m_updateNotifierWindow = UpdateNotifierWindow("Update available", true,
-		ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse);
-	PaletteEditorWindow  m_paletteEditorWindow  = PaletteEditorWindow("Palette Editor", true);
-	CustomHud            m_customHud            = CustomHud();
+	void InitWindows();
+	CustomHud m_customHud = CustomHud();
 
 	bool m_showCustomHud = false;
 	//ImVec2 m_middleScreen;
