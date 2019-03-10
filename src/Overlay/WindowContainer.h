67440a5ce23e794506eb5632c35465a330f1066a
kovidomi <kovidomi01@citromail.hu>
2019-03-10 23:29:25 +0100

    Change access modifier of GetWindows method

--- src/Overlay/WindowContainer.h
+++ src/Overlay/WindowContainer.h
@@ -12,6 +12,7 @@ public:
 	template <class T>
 	T* GetWindow(WindowType_ type) { return (T*)m_windows[type]; }
 	Window* GetWindow(WindowType_ type) { return m_windows[type]; }
+	const WindowMap& GetWindows() const { return m_windows; }
 	virtual ~WindowContainer()
 	{
 		for (const auto& window : m_windows)
@@ -24,7 +25,6 @@ protected:
 	virtual void FillWindowContainer() = 0;
 	void InitWindowContainer() { FillWindowContainer(); }
 	void AddWindow(WindowType_ type, Window* pWindow) { m_windows[type] = pWindow; }
-	const WindowMap& GetWindows() const { return m_windows; }
 private:
 	WindowMap m_windows;
 };
