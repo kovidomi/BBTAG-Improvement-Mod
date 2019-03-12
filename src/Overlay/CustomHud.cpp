dfd3f19e0fff3f9b25192bebe19e693e1f242c2d
kovidomi <kovidomi01@citromail.hu>
2019-03-12 22:53:01 +0100

    Comment out finished parts

--- src/Overlay/CustomHud.cpp
+++ src/Overlay/CustomHud.cpp
@@ -85,42 +85,42 @@ void CustomHud::OnUpdate(bool show_custom_hud, bool show_main_window)
 
 	//render top part of the hud
 	{
-		UpdateTimer(g_gameVals.pGameTimer);
+		//UpdateTimer(g_gameVals.pGameTimer);
 
-		if (p1Ch1Info.is_char_active)
-		{
-			UpdateHP(p1Ch1Info);
-			UpdateHP(p1Ch2Info);
-		}
-		else
-		{
-			UpdateHP(p1Ch2Info);
-			UpdateHP(p1Ch1Info);
-		}
+		//if (p1Ch1Info.is_char_active)
+		//{
+		//	UpdateHP(p1Ch1Info);
+		//	UpdateHP(p1Ch2Info);
+		//}
+		//else
+		//{
+		//	UpdateHP(p1Ch2Info);
+		//	UpdateHP(p1Ch1Info);
+		//}
 
-		if (p2Ch1Info.is_char_active)
-		{
-			UpdateHP(p2Ch1Info, true);
-			UpdateHP(p2Ch2Info, true);
-		}
-		else
-		{
-			UpdateHP(p2Ch2Info, true);
-			UpdateHP(p2Ch1Info, true);
-		}
+		//if (p2Ch1Info.is_char_active)
+		//{
+		//	UpdateHP(p2Ch1Info, true);
+		//	UpdateHP(p2Ch2Info, true);
+		//}
+		//else
+		//{
+		//	UpdateHP(p2Ch2Info, true);
+		//	UpdateHP(p2Ch1Info, true);
+		//}
 	}
 
 	//render bottom part of the hud
 	{
-		bool p1_is_astral_avail = IsAstralAvailable(p1Meters.cur_skill, p2Ch1Info.cur_hp, p2Ch2Info.cur_hp);
-		UpdateMeters(p1Meters.cur_skill, p1Meters.cur_cross,
-			p1Meters.cur_blaze, p1Meters.is_blaze_available,
-			p1_is_astral_avail, p1Meters.is_blaze_active, false);
+		//bool p1_is_astral_avail = IsAstralAvailable(p1Meters.cur_skill, p2Ch1Info.cur_hp, p2Ch2Info.cur_hp);
+		//UpdateMeters(p1Meters.cur_skill, p1Meters.cur_cross,
+		//	p1Meters.cur_blaze, p1Meters.is_blaze_available,
+		//	p1_is_astral_avail, p1Meters.is_blaze_active, false);
 
-		bool p2_is_astral_avail = IsAstralAvailable(p2Meters.cur_skill, p1Ch1Info.cur_hp, p1Ch2Info.cur_hp);
-		UpdateMeters(p2Meters.cur_skill, p2Meters.cur_cross,
-			p2Meters.cur_blaze, p2Meters.is_blaze_available, 
-			p2_is_astral_avail, p2Meters.is_blaze_active, true);
+		//bool p2_is_astral_avail = IsAstralAvailable(p2Meters.cur_skill, p1Ch1Info.cur_hp, p1Ch2Info.cur_hp);
+		//UpdateMeters(p2Meters.cur_skill, p2Meters.cur_cross,
+		//	p2Meters.cur_blaze, p2Meters.is_blaze_available, 
+		//	p2_is_astral_avail, p2Meters.is_blaze_active, true);
 
 		//render unique meters
 		{
@@ -219,205 +219,6 @@ bool CustomHud::UpdateVisibility()
 	return isOrigHUDHidden;
 }
 
-void CustomHud::UpdateHP(const CharInfo &charInfo, bool right_side)
-{
-	char wndtitle[36];
-	bool swap_bar_dir = false;
-
-	if (right_side)
-		sprintf(wndtitle, "P2_hp_gauge");
-	else
-	{
-		sprintf(wndtitle, "P1_hp_gauge");
-		swap_bar_dir = true;
-	}
-
-	ImGui::Begin(wndtitle, NULL, customHUDWindowFlags);
-
-	float cur_hp_percent = (float)charInfo.cur_hp / (float)charInfo.max_hp;
-
-	ImVec4 hp_color;
-	ImVec4 recoverable_hp_color(0.7f, 0.0f, 0.0f, 0.75f);
-
-	if (cur_hp_percent < 0.35f)
-		hp_color = ImVec4(1.0f, 0.25f, 0.0f, 1.0f);
-	else if (cur_hp_percent > 0.35f && cur_hp_percent < 1.0f)
-		hp_color = ImVec4(0.1f, 1.0f, 0.0f, 1.0f);
-	else
-		hp_color = ImVec4(0.0f, 0.85f, 1.0f, 1.0f);
-
-	//////////////////
-	float bar_width = 635.0f * hud_scale_x;
-	float bar_height = 45.0f * hud_scale_y;
-
-	//recoverable hp
-	float recoverable_hp_percent = 0.0f;
-	if (charInfo.recoverable_hp > 0)
-		recoverable_hp_percent = (float)(charInfo.recoverable_hp + charInfo.cur_hp) / (float)charInfo.max_hp;
-
-	ImGui::ColoredProgressBar(recoverable_hp_percent, ImVec2(bar_width, bar_height), recoverable_hp_color, NULL, false, swap_bar_dir);
-
-	//actual hp
-	//////////////////
-
-	//positioning so we render the next progress bar on top of the previous one
-	ImVec2 size = ImGui::GetItemRectSize();
-	ImGui::SetCursorPosY(ImGui::GetCursorPosY() - size.y - ImGui::GetStyle().ItemSpacing.y);
-
-	//char bufhp[32];
-	//sprintf(bufhp, "%d", charInfo.cur_hp);
-	ImGui::ColoredProgressBar(cur_hp_percent, ImVec2(bar_width, bar_height), hp_color, NULL, false, swap_bar_dir);
-
-	ImVec2 origPos = ImGui::GetCursorPos();
-
-	///////////////////////////
-	const char* charname;
-	if (charInfo.char_index < CHAR_NAMES_COUNT)
-		charname = charNames[charInfo.char_index].c_str();
-	else
-		charname = "<UNKNOWN>";
-
-	ImVec2 nameSize = ImGui::CalcTextSize(charname);
-	ImVec2 newPos;
-	if (right_side)
-		newPos = ImVec2((ImGui::GetCursorPosX() + size.x) - nameSize.x, (ImGui::GetCursorPosY() - size.y - ImGui::GetStyle().ItemSpacing.y));
-	else
-		newPos = ImVec2(ImGui::GetCursorPosX(), (ImGui::GetCursorPosY() - size.y - ImGui::GetStyle().ItemSpacing.y));
-
-	ImGui::SetCursorPos(newPos);
-	ImGui::Text("%s", charname);
-
-	//go back to original pos
-	ImGui::SetCursorPos(origPos);
-
-	ImGui::End();
-}
-
-void CustomHud::UpdateTimer(int *timer)
-{
-	if (!timer)
-		return;
-
-	ImGui::Begin("TIMER", NULL, customHUDWindowFlags);
-	ImGui::Text("TIME");
-
-	char time[32];
-	sprintf(time, "%d", *g_gameVals.pGameTimer);
-	ImGui::SetCursorPosX(ImGui::GetWindowSize().x / 2 - (ImGui::CalcTextSize(time).x / 2));
-	ImGui::Text(time);
-	ImGui::End();
-}
-
-void CustomHud::UpdateMeters(int cur_skill_val, int cur_cross_val, int cur_blaze_val, bool is_blaze_available, 
-	bool is_astral_available, bool is_blaze_active, bool right_side)
-{
-	ImVec2 skill_bar_size(200.0f * hud_scale_x, 35.0f * hud_scale_y);
-	ImVec2 cross_bar_size(400.0f * hud_scale_x, 40.0f * hud_scale_y);
-	char wndtitle[36];
-
-	bool swap_cross_bar_dir = false;
-	bool swap_skill_bar_dir = false;
-
-	if (right_side)
-	{
-		sprintf(wndtitle, "P2_meters");
-		swap_skill_bar_dir = true;
-		swap_cross_bar_dir = true;
-	}
-	else
-		sprintf(wndtitle, "P1_meters");
-
-	//set colors
-	if (is_astral_available)
-	{
-		ImGui::PushStyleColor(ImGuiCol_Border, astral_available_color);
-		ImGui::PushStyleColor(ImGuiCol_BorderShadow, astral_available_color);
-	}
-
-	if (is_blaze_active)
-	{
-		cross_bar_color = cross_bar_color_blaze_active;
-		skill_bar_color = skill_bar_color_blaze_active;
-	}
-	else if (is_blaze_available)
-	{
-		static float cross_blaze_prog = 0.0f, progress_dir = 1.0f;
-		const float BLINK_SPEED = 2.0f;
-		cross_blaze_prog += progress_dir * BLINK_SPEED * ImGui::GetIO().DeltaTime;
-		if (cross_blaze_prog >= +1.1f) { cross_blaze_prog = +1.1f; progress_dir *= -1.0f; }
-		if (cross_blaze_prog <= -0.1f) { cross_blaze_prog = -0.1f; progress_dir *= -1.0f; }
-
-		cross_bar_color.y = cross_blaze_prog;
-	}
-
-	ImGui::Begin(wndtitle, NULL, customHUDWindowFlags);
-
-	if (right_side)
-	{
-		char blaze_lvl[32];
-		char skill_lvl[32];
-		sprintf(blaze_lvl, "Lv %d", blaze_to_single_digit(cur_blaze_val));
-		sprintf(skill_lvl, "%d", total_skill_to_single_digit(cur_skill_val));
-
-		///////////////// POSITIONING
-		ImGui::TextColored(invis_color, blaze_lvl); //invis
-		ImGui::SameLine();
-
-		//push styles
-		ImGui::PushStyleColor(ImGuiCol_Border, invis_color); //invis
-		ImGui::PushStyleColor(ImGuiCol_FrameBg, invis_color); //invis
-
-															  //invis box
-		ImGui::ColoredProgressBar(total_skill_to_single_bar_percent(cur_skill_val, is_blaze_active),
-			skill_bar_size, invis_color, NULL, false);
-
-		ImGui::PopStyleColor(2);
-
-		ImGui::SameLine();
-		ImGui::SetCursorPosX(ImGui::GetCursorPosX() - ImGui::CalcTextSize(skill_lvl).x - (ImGui::GetStyle().ItemSpacing.x * 2));
-		//////////////////////////
-
-		/////////// SKILL BAR
-		ImGui::TextColored(skill_bar_color, skill_lvl);
-		ImGui::SameLine();
-		ImGui::ColoredProgressBar(total_skill_to_single_bar_percent(cur_skill_val, is_blaze_active),
-			skill_bar_size, skill_bar_color, NULL, false, swap_skill_bar_dir);
-
-		////////// BLAZE VAL
-		ImGui::TextColored(blaze_val_color, blaze_lvl);
-		ImGui::SameLine();
-
-		////////// CROSS BAR
-		ImGui::ColoredProgressBar(total_cross_to_bar_percent(cur_cross_val),
-			cross_bar_size, cross_bar_color, NULL, false, swap_cross_bar_dir);
-	}
-	else
-	{
-		/////////// SKILL BAR
-		ImGui::ColoredProgressBar(total_skill_to_single_bar_percent(cur_skill_val, is_blaze_active),
-			skill_bar_size, skill_bar_color, NULL, false, swap_skill_bar_dir);
-
-		ImGui::SameLine();
-		ImGui::TextColored(skill_bar_color, "%d", total_skill_to_single_digit(cur_skill_val));
-
-		////////// CROSS BAR
-		ImGui::ColoredProgressBar(total_cross_to_bar_percent(cur_cross_val),
-			cross_bar_size, cross_bar_color, NULL, false, swap_cross_bar_dir);
-
-		////////// BLAZE VAL
-		ImGui::SameLine();
-		ImGui::TextColored(blaze_val_color, "Lv %d", blaze_to_single_digit(cur_blaze_val));
-	}
-
-	//return the bar colors back to their default colors after done
-	cross_bar_color = default_cross_bar_color;
-	skill_bar_color = default_skill_bar_color;
-	if (is_astral_available)
-		ImGui::PopStyleColor(2);
-
-	ImGui::End();
-}
-
 void CustomHud::UpdateCharSpecificMeters(const CharInfo & charInfo, bool right_side)
 {
 
@@ -511,12 +312,4 @@ void CustomHud::UpdateCharSpecificMeters(const CharInfo & charInfo, bool right_s
 	}
 
 	ImGui::End();
-}
-
-bool CustomHud::IsAstralAvailable(const int curSkill, const int otherCharHP1, const int otherCharHP2)
-{
-	if (curSkill >= SKILL_GAUGE_MAX_VALUE_BLAZE_ACTIVE && (otherCharHP1 <= 0 || otherCharHP2 <= 0))
-		return true;
-
-	return false;
 }
--- src/Overlay/CustomHud.h
+++ src/Overlay/CustomHud.h
@@ -11,12 +11,11 @@ public:
 	void ShowResetPositionsButton(ImVec2 middlescreen);
 private:
 	bool UpdateVisibility();
-	void UpdateHP(const CharInfo &charInfo, bool right_side = false);
-	void UpdateTimer(int *timer);
-	void UpdateMeters(int cur_skill_val, int cur_cross_val, int cur_blaze_val,
-		bool is_blaze_available, bool is_astral_available, bool is_blaze_active, bool right_side = false);
+	// void UpdateHP(const CharInfo &charInfo, bool right_side = false);
+	// void UpdateTimer(int *timer);
+	//void UpdateMeters(int cur_skill_val, int cur_cross_val, int cur_blaze_val,
+	//	bool is_blaze_available, bool is_astral_available, bool is_blaze_active, bool right_side = false);
 	void UpdateCharSpecificMeters(const CharInfo &charInfo, bool right_side = false);
-	bool IsAstralAvailable(const int curSkill, const int otherCharHP1, const int otherCharHP2);
 
 	float hud_scale_x = 0;
 	float hud_scale_y = 0;
