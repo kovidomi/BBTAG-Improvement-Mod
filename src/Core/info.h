205840d9816fd2b624b3b0a0e5d9e8d0a1817a10
kovidomi <kovidomi01@citromail.hu>
2019-05-22 21:40:27 +0200

    Change version number to v1.07

--- src/Core/info.h
+++ src/Core/info.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define MOD_VERSION_NUM	"v1.06"
-#define MOD_VERSION_NUM_INTERNAL 106
+#define MOD_VERSION_NUM	"v1.07"
+#define MOD_VERSION_NUM_INTERNAL 107
 #define MOD_WINDOW_TITLE "BBTAG Improvement Mod"
 
 //Links
