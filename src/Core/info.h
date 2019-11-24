ee0a1b4c126c2f6bd55b0f4f3d5a6b8d334d1d30
kovidomi <kovidomi01@citromail.hu>
2019-11-24 02:16:23 +0100

    Add md5 hash check for palette archive download #10

--- src/Core/info.h
+++ src/Core/info.h
@@ -12,3 +12,4 @@
 #define MOD_LINK_DONATE L"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=C8EDR78DJ8PU2"
 #define MOD_LINK_DONATORS L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/donators.txt"
 #define MOD_LINK_PALETTES_ARCHIVE L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/palettes.tar.gz"
+#define MOD_LINK_PALETTES_HASH L"https://github.com/kovidomi/BBTAG-Improvement-Mod/raw/master/resource/palettes_hash.txt"
